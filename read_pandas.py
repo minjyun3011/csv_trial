622aa4fab954f47242884105955b2549fe789963
minjyun3011 <grand3canyon10@gmail.com>
2023-12-18 14:03:15 +0900

    日にちとその日の検査実施件数のみの表示完了

--- read_pandas.py
+++ read_pandas.py
@@ -2,14 +2,22 @@ import pandas as pd
 
 df = pd.read_csv(
     "data/193301.csv",
-    usecols=[0, 1, 2, 3, 4, 5, 6],
+    usecols=[0, 4],
     delimiter=",",
-    dtype={"実施_年月日": "str", "全国地方公共団体コード": "str", "都道府県名": "str", "市区町村名": "str", "実施_件数": "Int64", "備考": "str", "【参考】件数（県）": "Int64"},
+    dtype={"実施_年月日": "str", "検査実施_件数": "float"},
     encoding="utf-8-sig",
     thousands=',',  # コンマを取り除く
     na_values=["-"],  # "-" を欠損値として扱う
 )
 
+# 実施_年月日の文字列を日付型に変換
 df['実施_年月日'] = pd.to_datetime(df["実施_年月日"], format='%Y-%m-%d')
 
-print(df)
+# 実施_年月日の表示を「年月日」形式に変更
+df['実施_年月日'] = df['実施_年月日'].dt.strftime('%Y年%m月%d日')
+
+# 実施_件数の値を整数に変換して、文字列としてフォーマット
+df['検査実施_件数'] = df['検査実施_件数'].apply(lambda x: f"{int(x):,}件" if pd.notnull(x) else "")
+
+# 最初の57行だけ表示
+print(df.head(56))
